25e0e0ff1172af199e8c03a248d732eecec11b22
mohamed mamdouh <mamdouhibr67@gmail.com>
2026-04-23 02:48:59 +0200

    Refactor flight-booking-app and my-nest-app configurations, update proxy settings, and enhance code style configurations

--- apps/flight-booking-app/src/app/app.ts
+++ apps/flight-booking-app/src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject, OnInit, PLATFORM_ID, PlatformRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
-  imports: [ RouterModule],
+  standalone: true,
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
-export class App {
-  protected title = 'flight-booking-app';
+export class App implements OnInit {
+
+  constructor( private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {
+    
+
+  }
+ngOnInit(): void {
+  if(isPlatformBrowser(this.platformId)){
+    this.http.get('/api?to=world').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.error(error)
+    });
+  }
+}
 }
--- apps/my-nest-app/src/app/app.controller.ts
+++ apps/my-nest-app/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  getData(@Query('to') query: any){
+    return { query };
   }
 }
